7d99e49d246f74bea52158a5b7f2fdbe5eacaf25
xpall <contact@johnlloyd.dev>
2026-01-17 15:14:49 +0800

    Remove hardcoded barangay data, import data from data/government.ts

--- react-app/src/components/government/BarangayList.tsx
+++ react-app/src/components/government/BarangayList.tsx
@@ -1,30 +1,17 @@
 import BarangayCard from './BarangayCard'
+import { barangaysData } from '@/data/government'
 
 export default function BarangayList() {
   return (
     <div className="grid grid-4" style={{ gap: 'var(--spacing-sm)' }}>
-      <BarangayCard name="Aggub" captain="Kap. Felix Caramat Tolentino Sr." phone="0975 177 2529" />
-      <BarangayCard name="Bagahabag" captain="Kap. Eduardo Abu Bangad Sr." phone="0936 655 1984" />
-      <BarangayCard name="Bangaan" captain="Kap. Isidro Baliza Prado" phone="0935 242 5023" />
-      <BarangayCard name="Bangar" captain="Kap. Victor Tubay Adalin Jr." phone="0935 806 6847" />
-      <BarangayCard name="Bascaran" captain="Kap. Federico Vienes Asuncion" phone="0926 187 9385" />
-      <BarangayCard name="Communal" captain="Kap. Renaldo Gin-Om Nabad-Aw" phone="0905 841 1484" />
-      <BarangayCard name="Concepcion" captain="Kap. Jenny Gomez Marquez" phone="0927 464 6243" />
-      <BarangayCard name="Curifang" captain="Kap. Elmer Zabala Lingayu" phone="0936 360 7880" />
-      <BarangayCard name="Dadap" captain="Kap. Rudy Eugenio Paranis" phone="0905 341 2755" />
-      <BarangayCard name="Lactawan" captain="Kap. Sherwin Elevazo Vicente" phone="0916 995 8916" />
-      <BarangayCard name="Osmeña" captain="Kap. Danilo Estacio Domingo" phone="0906 384 3974" />
-      <BarangayCard name="Pilar D. Galima" captain="Kap. Dexter Yaranon Ruiz" phone="0965 551 6034" />
-      <BarangayCard name="Poblacion North" captain="Kap. Angelito De Leon Tiongson" phone="0917 147 5710" />
-      <BarangayCard name="Poblacion South" captain="Kap. Melchor Esmedina Marzo" phone="0906 268 5095" />
-      <BarangayCard name="Quezon" captain="Kap. Luis Amangi Castillo" phone="0920 945 4538" />
-      <BarangayCard name="Quirino" captain="Kap. Walter Dalupang Savedra Sr." phone="0935 198 7103" />
-      <BarangayCard name="Roxas" captain="Kap. Edwin Bungan Dacayo" phone="0936 450 4413" />
-      <BarangayCard name="San Juan" captain="Kap. Nardo Eustaquio Ramel" phone="0965 348 0556" />
-      <BarangayCard name="San Luis" captain="Kap. Ernesto Domingo Ramones Jr." phone="0936 368 0681" />
-      <BarangayCard name="Tucal" captain="Kap. Roberto Ojastro Dizon" phone="0966 176 9302" />
-      <BarangayCard name="Uddiawan" captain="Kap. Jessie Concepcion Delos Reyes" phone="0935 546 0625" />
-      <BarangayCard name="Wacal" captain="Kap. Janette Quines Cristobal" phone="0915 962 1403" />
+      {barangaysData.map((barangay, index) => (
+        <BarangayCard
+          key={index}
+          name={barangay.name}
+          captain={barangay.captain}
+          phone={barangay.phone}
+        />
+      ))}
     </div>
   )
 }
